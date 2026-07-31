60ff0c57ce387975680f41f2ae3189096c69c42c
siddhi gaikwad <siddhigaikwad6677@gmail.com>
2026-07-31 14:24:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Welcome message
+window.onload = function () {
+    alert("Welcome to Smart Agro Care 🌱");
+};
+
+// Soil button
+function checkSoil() {
+    alert("Soil Quality Analysis feature will be available soon.");
+}
+
+// Crop Disease button
+function cropDisease() {
+    alert("Crop Disease Detection feature will be available soon.");
+}
